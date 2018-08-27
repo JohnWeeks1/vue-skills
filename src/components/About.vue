028cc7e2ed76a68db8cd8790f3fce92e0a5d56cb
JohnWeeks1 <jonathanweeks007@gmail.com>
2018-08-27 16:02:31 +0100

    First Commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+      <h1>Hello {{user_name}}, This is an about page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo libero sunt repudiandae quis quos quam doloremque tenetur, in assumenda dolore et voluptate placeat, fuga cum officia laudantium eos excepturi aperiam?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      user_name: this.$route.params.name
+    }
+  }
+}
+</script>
